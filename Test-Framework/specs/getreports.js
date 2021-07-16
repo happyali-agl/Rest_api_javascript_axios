afc85bf9f335d0e8c49d6341d1363f46212ab684
Mohammed <mirmohammed.ali@tafensw.com.au>
2021-07-16 15:13:42 +1000

    updated Hobson failed test case

--- Test-Framework/specs/getreports.js
+++ Test-Framework/specs/getreports.js
@@ -8,7 +8,8 @@ const {via_location} = require("../apiModules/qantasApp/via_location");
 
 describe('Get Reports', function() {
 this.timeout(30000);
-    describe('Current Weather via Location', function() {
+	describe('Current Weather via Location', function() {
+		describe('Current Weather via Location', function() {
 			it('Details for city Heathsville', async function() {
 				// reqData = envData.BaseURL1+envData.endPoints.location; //.replace(('lat',19),('lon',76));
 				reqData = envData.BaseURL+envData.endPoints.version+"?"+"lat="+location.Heathsville.lat+"&"+"lon="+location.Heathsville.lon+"&"+"key="+envData.endPoints.key;
@@ -22,11 +23,12 @@ this.timeout(30000);
 			});
 			it('Details for city Noma', async function() {
 				// reqData = envData.BaseURL1+envData.endPoints.location; //.replace(('lat',19),('lon',76));
-				reqData = envData.BaseURL+envData.endPoints.version+"?"+"lat="+location.Heathsville.lat+"&"+"lon="+location.Heathsville.lon+"&"+"key="+envData.endPoints.key;
+				reqData = envData.BaseURL+envData.endPoints.version+"?"+"lat="+location.Noma.lat+"&"+"lon="+location.Heathsville.lon+"&"+"key="+envData.endPoints.key;
 				currentResponse = await via_location(reqData);
 				expect(currentResponse.status).to.equal(200);          
 				currentResponseBody = JSON.parse(JSON.stringify(currentResponse.data));    
-				expect(currentResponseBody).to.be.an('object');  
+				expect(currentResponseBody).to.be.an('object');   
+				expect(currentResponseBody.data[0].city_name).to.equal(location.Noma.city_name);
 				report(this, 'Actual Response Data', currentResponseBody);        
 			});
 			it('Request with Invalid Key', async function() {
@@ -36,11 +38,13 @@ this.timeout(30000);
 				expect(currentResponse.status).to.equal(403);          
 				currentResponseBody = JSON.parse(JSON.stringify(currentResponse.data));    
 				expect(currentResponseBody).to.be.an('object');
+				report(this, 'Error ', currentResponseBody.error);
+				expect(currentResponseBody.data[0].city_name).to.equal(location.Heathsville.errorMsg); 
 				report(this, 'Actual Response Data', currentResponseBody);              
+			});
 		});
-    });
 
-	describe('Current Weather via postcode', function() {
+		describe('Current Weather via postcode', function() {
 			it('Postcode for New Delhi', async function() {
 					// reqData = envData.BaseURL1+envData.endPoints.postcode;
 					reqData = envData.BaseURL+envData.endPoints.version+"?"+"postal_code="+location.NewDelhi.postcode+"&"+"key="+envData.endPoints.key;
@@ -68,5 +72,6 @@ this.timeout(30000);
 				expect(currentResponseBody).to.be.an('object');    
 				report(this, 'Actual Response Data', currentResponseBody);
 			});
+		});
 	});
 });
